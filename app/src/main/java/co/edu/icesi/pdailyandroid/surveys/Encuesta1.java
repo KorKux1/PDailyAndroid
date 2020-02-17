9dc4b61dcfa60ad004f6e0ae9bd4bd9e8a25776f
DOMICIANO RINCON <domiciano.rincon@correounivalle.edu.co>
2020-02-17 14:26:11 -0500

    Actividad Creada

---
+++ app/src/main/java/co/edu/icesi/pdailyandroid/surveys/Encuesta1.java
@@ -0,0 +1,15 @@
+package co.edu.icesi.pdailyandroid.surveys;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.edu.icesi.pdailyandroid.R;
+
+import android.os.Bundle;
+
+public class Encuesta1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encuesta1);
+    }
+}
