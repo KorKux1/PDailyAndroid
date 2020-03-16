3b2b7734d69428967e285b8cf973d1db93ccc327
Catalina <catalalina19@gmail.com>
2020-03-15 21:28:26 -0500

    Working on logic for template A

--- app/src/main/java/co/edu/icesi/pdailyandroid/FormActivity.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/FormActivity.java
@@ -1,16 +1,23 @@
 package co.edu.icesi.pdailyandroid;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 import java.lang.reflect.Type;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import co.edu.icesi.pdailyandroid.cognosis.builder.Form;
 import co.edu.icesi.pdailyandroid.cognosis.fragments.TypeA;
 import co.edu.icesi.pdailyandroid.services.App;
@@ -18,11 +25,13 @@ import co.edu.icesi.pdailyandroid.util.JsonReaderUtils;
 
 public class FormActivity extends AppCompatActivity {
 
-    String json_form = JsonReaderUtils.getJsonFromAssets(App.getAppContext(),"pd-cfrs.json");
+    String json_form = JsonReaderUtils.getJsonFromAssets(App.getAppContext(), "pd-cfrs.json");
     Gson gson = new Gson();
-    Type form_type = new TypeToken<Form>() { }.getType();
-
+    Type form_type = new TypeToken<Form>() {
+    }.getType();
     LinearLayout fragmentContainer;
+    int index = 0;
+    Form form;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +39,113 @@ public class FormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_form);
         Log.i("data", json_form);
 
-        Form form = gson.fromJson(json_form, form_type);
+        form = gson.fromJson(json_form, form_type);
 
         Log.i("JSON OBJECT", form.toString());
 
-        fragmentContainer = findViewById(R.id.fragmentContainer);
+
+//        switch (form.getForm_questions()[index].getQuestion_type()) {
+//            case "A":
+//
+//                break;
+//        }
+        updateFragmentTypeA(form, index);
+    }
 
 
-        if( form.getForm_questions()[0].getQuestion_type().equals("A") ) {
+    protected void onStart(){
+        super.onStart();
+        setContentView(R.layout.activity_form);
+        updateFragmentTypeA(form, index);
+    }
 
-            TypeA typeA = new TypeA();
-            typeA.setText(form.getForm_questions()[0].getQuestion_text());
-            typeA.setIdeal(form.getForm_questions()[0].getQuestion_ideal());
 
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.replace(R.id.fragmentContainer, typeA);
-            transaction.commit();
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_form);
+
+        Button previous = findViewById(R.id.ButtonPrevious);
+        Button next = findViewById(R.id.ButtonNext);
+
+
+        if (index == 0) {
+            previous.setVisibility(View.GONE);
         }
 
+
+        if (index + 1 == form.getForm_questions().length) {
+            next.setVisibility(View.GONE);
+        }
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index = index - 1;
+                switch (form.getForm_questions()[index].getQuestion_type()) {
+                    case "A":
+                        updateFragmentTypeA(form, index);
+                        break;
+                }
+
+                if (index == 0) {
+                    previous.setVisibility(View.GONE);
+                }
+                if (index > 0) {
+                    previous.setVisibility(View.VISIBLE);
+                }
+                if (index < form.getForm_questions().length) {
+                    next.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index = index + 1;
+                Log.i("yeeeeeeeeeeeeees", Integer.valueOf(index).toString());
+
+                switch (form.getForm_questions()[index].getQuestion_type()) {
+                    case "A":
+                        updateFragmentTypeA(form, index);
+                        break;
+                }
+                if (index > 0) {
+                    previous.setVisibility(View.VISIBLE);
+                }
+                if (index + 1 == form.getForm_questions().length) {
+                    next.setVisibility(View.GONE);
+                }
+            }
+        });
     }
+
+    protected void updateFragmentTypeA(Form form, int index) {
+        fragmentContainer = findViewById(R.id.fragmentContainer);
+        TextView formDescription = findViewById(R.id.TextFormDescription);
+        TextView formName = findViewById(R.id.TextFormName);
+        TextView formQNumber = findViewById(R.id.TextFormQNumber);
+        TextView formTotal = findViewById(R.id.TextFormTotal);
+
+        TypeA typeA = new TypeA();
+        formDescription.setText(form.getForm_description());
+        formName.setText(form.getForm_name());
+        formTotal.setText(Integer.valueOf(form.getForm_questions().length).toString());
+        formQNumber.setText(Integer.valueOf(index + 1).toString());
+        typeA.setFormQuestion(form.getForm_questions()[index].getQuestion_text());
+
+        typeA.setAnswerOne(form.getForm_questions()[index].getQuestion_options()[0]);
+        typeA.setAnswerTwo(form.getForm_questions()[index].getQuestion_options()[1]);
+        typeA.setAnswerThree(form.getForm_questions()[index].getQuestion_options()[2]);
+        typeA.setAnswerFour(form.getForm_questions()[index].getQuestion_options()[3]);
+
+        typeA.setIndex(index);
+        typeA.setFormTotalNumber(form.getForm_questions().length);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragmentContainer, typeA);
+        transaction.commit();
+    }
+
 }
--- app/src/main/java/co/edu/icesi/pdailyandroid/cognosis/fragments/TypeA.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/cognosis/fragments/TypeA.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import co.edu.icesi.pdailyandroid.R;
 
@@ -17,8 +19,18 @@ import co.edu.icesi.pdailyandroid.R;
  */
 public class TypeA extends Fragment {
 
-    private String text;
-    private String ideal;
+//    private String text;
+//    private String ideal;
+
+    private  String formQuestion;
+
+    private  String answerOne;
+    private  String answerTwo;
+    private  String answerThree;
+    private  String answerFour;
+
+    private int formTotalNumber;
+    private int index;
 
     public TypeA() {
         // Required empty public constructor
@@ -30,20 +42,54 @@ public class TypeA extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_type_a, container, false);
-        EditText pregunta1 = view.findViewById(R.id.pregunta1);
-        EditText pregunta2 = view.findViewById(R.id.pregunta2);
+//        EditText pregunta1 = view.findViewById(R.id.pregunta1);
+//        EditText pregunta2 = view.findViewById(R.id.pregunta2);
 
-        pregunta1.setText(this.text);
-        pregunta2.setText(this.ideal);
+//        pregunta1.setText(this.text);
+//        pregunta2.setText(this.ideal);
+
+        TextView formQuestion = view.findViewById(R.id.TextFormQuestion);
+
+        Button answerOne = view.findViewById(R.id.ButtonAnswerOne);
+        Button answerTwo = view.findViewById(R.id.ButtonAnswerTwo);
+        Button answerThree = view.findViewById(R.id.ButtonAnswerThree);
+        Button answerFour = view.findViewById(R.id.ButtonAnswerFour);
+
+        formQuestion.setText(this.formQuestion);
+
+        answerOne.setText(this.answerOne);
+        answerTwo.setText(this.answerTwo);
+        answerThree.setText(this.answerThree);
+        answerFour.setText(this.answerFour);
 
         return view;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setFormQuestion(String formQuestion) {
+        this.formQuestion = formQuestion;
     }
 
-    public void setIdeal(String ideal) {
-        this.ideal = ideal;
+    public void setAnswerOne(String answerOne) {
+        this.answerOne = answerOne;
+    }
+
+    public void setAnswerTwo(String answerTwo) {
+        this.answerTwo = answerTwo;
+    }
+
+    public void setAnswerThree(String answerThree) {
+        this.answerThree = answerThree;
+    }
+
+    public void setAnswerFour(String answerFour) {
+        this.answerFour = answerFour;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setFormTotalNumber(int formTotalNumber) {
+        this.formTotalNumber = formTotalNumber;
     }
 }
