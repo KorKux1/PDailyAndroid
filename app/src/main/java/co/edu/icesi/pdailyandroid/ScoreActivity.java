8aa689310b28776d440b4a6b51f643d8d62c4c20
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2020-07-02 00:22:00 -0500

    Working on DataScore Connections and ScoreActivity display data.

--- app/src/main/java/co/edu/icesi/pdailyandroid/ScoreActivity.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/ScoreActivity.java
@@ -19,6 +19,8 @@ public class ScoreActivity extends AppCompatActivity {
     private Button chao;
 
     private TextView scoreNumber;
+    private TextView tv_score_time_total;
+    private TextView tv_score_time_average;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +34,32 @@ public class ScoreActivity extends AppCompatActivity {
 //        Log.i("EXTRA_TYPE", type);
 
         scoreNumber = findViewById(R.id.firstSection);
+        tv_score_time_total = findViewById(R.id.tv_score_time_total);
+        tv_score_time_average = findViewById(R.id.tv_score_time_average);
 
         switch (type) {
             case "PD-NMS":
                 scoreNumber.setText(Integer.valueOf(dataScore.getForm_score_pdnms()).toString());
+                tv_score_time_total.setText(String.valueOf(dataScore.getForm_time_response_pdnms_total()) + " " + "Min");
+                tv_score_time_average.setText(String.valueOf(dataScore.getForm_time_response_pdnms_total()/dataScore.getForm_answers_pdnms().length) + " " + "Min/Pregunta");
                 break;
 
             case "PD-CFRS":
                 scoreNumber.setText(Integer.valueOf(dataScore.getForm_score_pdcfrs()).toString());
+                tv_score_time_total.setText(String.valueOf(dataScore.getForm_time_response_pdcfrs_total()) + " " + "Min");
+                tv_score_time_average.setText(String.valueOf(dataScore.getForm_time_response_pdcfrs_total()/dataScore.getForm_answers_pdcfrs().length) + " " + "Min/Pregunta");
                 break;
 
             case "Congelamiento de la marcha":
                 scoreNumber.setText(Integer.valueOf(dataScore.getForm_score_walk()).toString());
+                tv_score_time_total.setText(String.valueOf(dataScore.getForm_time_response_walk_total()) + " " + "Min");
+                tv_score_time_average.setText(String.valueOf(dataScore.getForm_time_response_walk_total()/dataScore.getForm_answers_walk().length) + " " + "Min/Pregunta");
                 break;
 
             case "PHQ-9":
                 scoreNumber.setText(Integer.valueOf(dataScore.getForm_score_phq9()).toString());
+                tv_score_time_total.setText(String.valueOf(dataScore.getForm_time_response_phq9_total()) + " " + "Min");
+                tv_score_time_average.setText(String.valueOf(dataScore.getForm_time_response_phq9_total()/dataScore.getForm_answers_phq9().length) + " " + "Min/Pregunta");
                 break;
 
             case "MoCA":
