3ad343ec9fe5438eabc957ae53bdfdf8df3b82e1
Leonardo Vargas <lvbernal@gmail.com>
2022-08-02 17:41:09 -0500

    load levo schedules

--- app/src/main/java/co/edu/icesi/pdailyandroid/model/dto/MedicineLevodopaScheduleDTO.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/model/dto/MedicineLevodopaScheduleDTO.java
@@ -28,4 +28,68 @@ public class MedicineLevodopaScheduleDTO {
         this.scheduledDose = scheduledDose;
         this.metadata = metadata;
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(UUID patientId) {
+        this.patientId = patientId;
+    }
+
+    public UUID getMedicineLevodopaTypeId() {
+        return medicineLevodopaTypeId;
+    }
+
+    public void setMedicineLevodopaTypeId(UUID medicineLevodopaTypeId) {
+        this.medicineLevodopaTypeId = medicineLevodopaTypeId;
+    }
+
+    public String getMedicineLevodopaTypeLabel() {
+        return medicineLevodopaTypeLabel;
+    }
+
+    public void setMedicineLevodopaTypeLabel(String medicineLevodopaTypeLabel) {
+        this.medicineLevodopaTypeLabel = medicineLevodopaTypeLabel;
+    }
+
+    public double getMedicineLevodopaTypeQuantity() {
+        return medicineLevodopaTypeQuantity;
+    }
+
+    public void setMedicineLevodopaTypeQuantity(double medicineLevodopaTypeQuantity) {
+        this.medicineLevodopaTypeQuantity = medicineLevodopaTypeQuantity;
+    }
+
+    public String getMedicineLevodopaUnitsTypeLabel() {
+        return medicineLevodopaUnitsTypeLabel;
+    }
+
+    public void setMedicineLevodopaUnitsTypeLabel(String medicineLevodopaUnitsTypeLabel) {
+        this.medicineLevodopaUnitsTypeLabel = medicineLevodopaUnitsTypeLabel;
+    }
+
+    public double getScheduledDose() {
+        return scheduledDose;
+    }
+
+    public void setScheduledDose(double scheduledDose) {
+        this.scheduledDose = scheduledDose;
+    }
+
+    public SchedulePlanDTO getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(SchedulePlanDTO metadata) {
+        this.metadata = metadata;
+    }
 }
--- app/src/main/java/co/edu/icesi/pdailyandroid/viewcontrollers/LevoFragment.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/viewcontrollers/LevoFragment.java
@@ -1,12 +1,20 @@
 package co.edu.icesi.pdailyandroid.viewcontrollers;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import co.edu.icesi.pdailyandroid.R;
+import co.edu.icesi.pdailyandroid.model.dto.MedicineLevodopaScheduleDTO;
+import co.edu.icesi.pdailyandroid.model.dto.SchedulesCollectionDTO;
+import co.edu.icesi.pdailyandroid.services.SessionManager;
 
 
 public class LevoFragment extends Fragment {
@@ -15,14 +23,18 @@ public class LevoFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        SessionManager sessionManager = new SessionManager(
+                getActivity().getApplicationContext());
+        SchedulesCollectionDTO schedules = sessionManager.loadSchedulesData();
+        ArrayList<MedicineLevodopaScheduleDTO> levoSchedules = schedules.getMedicineLevodopaSchedules();
+
+        for (MedicineLevodopaScheduleDTO s : levoSchedules) {
+            Log.w("log", s.getMedicineLevodopaTypeLabel());
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_levo, container, false);
     }
-
-
 }
