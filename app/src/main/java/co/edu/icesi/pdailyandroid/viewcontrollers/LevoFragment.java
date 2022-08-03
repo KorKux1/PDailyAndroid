fc3343fac3491ab1d2f4b6e948b42c4ecc1fb019
Leonardo Vargas <lvbernal@gmail.com>
2022-08-02 21:50:33 -0500

    levo fragment

--- app/src/main/java/co/edu/icesi/pdailyandroid/viewcontrollers/LevoFragment.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/viewcontrollers/LevoFragment.java
@@ -4,20 +4,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 import co.edu.icesi.pdailyandroid.R;
+import co.edu.icesi.pdailyandroid.adapters.LevoAdapter;
 import co.edu.icesi.pdailyandroid.model.dto.MedicineLevodopaScheduleDTO;
 import co.edu.icesi.pdailyandroid.model.dto.SchedulesCollectionDTO;
 import co.edu.icesi.pdailyandroid.services.SessionManager;
 
 
 public class LevoFragment extends Fragment {
+
+    private ListView levoTable;
+    private LevoAdapter adapter;
+    private ArrayList<MedicineLevodopaScheduleDTO> list;
 
     public LevoFragment() {
         // Required empty public constructor
@@ -26,15 +31,21 @@ public class LevoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_levo, container, false);
+        levoTable = v.findViewById(R.id.levoTable);
+        list = new ArrayList<>();
+        adapter = new LevoAdapter(list);
+        levoTable.setAdapter(adapter);
+
         SessionManager sessionManager = new SessionManager(
                 getActivity().getApplicationContext());
         SchedulesCollectionDTO schedules = sessionManager.loadSchedulesData();
         ArrayList<MedicineLevodopaScheduleDTO> levoSchedules = schedules.getMedicineLevodopaSchedules();
 
-        for (MedicineLevodopaScheduleDTO s : levoSchedules) {
-            Log.w("log", s.getMedicineLevodopaTypeLabel());
-        }
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_levo, container, false);
+        list.addAll(levoSchedules);
+        adapter.notifyDataSetChanged();
+
+        return v;
     }
 }
