ad6b7eac59d8ebdd11336037dc122a39a2976627
Leonardo Vargas <lvbernal@gmail.com>
2022-08-18 22:50:45 -0500

    alarm receiver supports levo

--- app/src/main/java/co/edu/icesi/pdailyandroid/receivers/broadcast/AlarmReceiver.java
+++ app/src/main/java/co/edu/icesi/pdailyandroid/receivers/broadcast/AlarmReceiver.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 import co.edu.icesi.pdailyandroid.app.App;
 import co.edu.icesi.pdailyandroid.localdatabase.DataHandler;
-import co.edu.icesi.pdailyandroid.model.viewmodel.NotificationFoodFollowUp;
+import co.edu.icesi.pdailyandroid.model.viewmodel.NotificationFollowUp;
 import co.edu.icesi.pdailyandroid.util.NotificationUtils;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -27,21 +27,27 @@ public class AlarmReceiver extends BroadcastReceiver {
         switch (type) {
             case "FOOD01":
                 notificationNumber = 1;
-                NotificationFoodFollowUp breakfast = new NotificationFoodFollowUp(UUID.randomUUID().toString(), "¿Ya desayunaste?", date);
+                NotificationFollowUp breakfast = new NotificationFollowUp(UUID.randomUUID().toString(), "¿Ya desayunaste?", date);
                 DataHandler.getInstance(App.getAppContext()).insertOrUpdateFoodNotification(breakfast);
                 NotificationUtils.createSimpleNotification(context, notificationNumber, "PDaily", "¿Ya desayunaste?", i);
                 break;
             case "FOOD02":
                 notificationNumber = 2;
-                NotificationFoodFollowUp lunch = new NotificationFoodFollowUp(UUID.randomUUID().toString(), "¿Ya almorzaste?", date);
+                NotificationFollowUp lunch = new NotificationFollowUp(UUID.randomUUID().toString(), "¿Ya almorzaste?", date);
                 DataHandler.getInstance(App.getAppContext()).insertOrUpdateFoodNotification(lunch);
                 NotificationUtils.createSimpleNotification(context, notificationNumber, "PDaily", "¿Ya almorzaste?", i);
                 break;
             case "FOOD03":
                 notificationNumber = 3;
-                NotificationFoodFollowUp dinner = new NotificationFoodFollowUp(UUID.randomUUID().toString(), "¿Ya cenaste?", date);
+                NotificationFollowUp dinner = new NotificationFollowUp(UUID.randomUUID().toString(), "¿Ya cenaste?", date);
                 DataHandler.getInstance(App.getAppContext()).insertOrUpdateFoodNotification(dinner);
                 NotificationUtils.createSimpleNotification(context, notificationNumber, "PDaily", "¿Ya cenaste?", i);
+                break;
+            case "LEVO":
+                notificationNumber = 4;
+                NotificationFollowUp levo = new NotificationFollowUp(UUID.randomUUID().toString(), "¿Ya tomaste tu medicina?", date);
+                DataHandler.getInstance(App.getAppContext()).insertOrUpdateLevoNotification(levo);
+                NotificationUtils.createSimpleNotification(context, notificationNumber, "PDaily", "¿Ya tomaste tu medicina?", i);
                 break;
         }
     }
